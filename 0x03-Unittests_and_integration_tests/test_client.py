e177eb5bd84937156e8fe01fa569528f64ae5667
JesutofunmiOludu <130249061+JesutofunmiOludu@users.noreply.github.com>
2025-11-12 16:53:14 +0100

    Create test_client.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch, PropertyMock
+import requests
+from client import GithubOrgClient
+
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """Test org method"""
+        mock_get_json.return_value = {"org": org_name}
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, {"org": org_name})
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
+
+if __name__ == "__main__":
+    unittest.main()
