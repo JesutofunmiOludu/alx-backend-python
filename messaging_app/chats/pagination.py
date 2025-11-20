73a1bf642199a0c73a10cef4cee29d12f96ad929
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2025-11-20 11:32:35 +0100

    update on paging

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -5,3 +5,13 @@ class MessagePagination(pagination.PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            "count": self.page.paginator.count,
+            "total_pages": self.page.paginator.num_pages,
+            "current_page": self.page.number,
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+            "results": data
+        })
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -54,7 +54,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication"
     ],
 
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+
     'PAGE_SIZE': 20,
 
     'DEFAULT_FILTER_BACKENDS': [
